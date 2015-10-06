a4dded91b3bd22fd5fadb016d8fc572bc7f0b700
Baptiste Clavié <clavie.b@gmail.com>
2015-10-06 18:10:39 +0200

    Add Json context

---
+++ context/AbstractJson.php
@@ -0,0 +1,207 @@
+<?php
+namespace Wisembly\Behat\Context;
+
+use Datetime;
+
+use Behat\Gherkin\Node\PyStringNode;
+
+use PHPUnit_Framework_Assert as Assert;
+
+use Symfony\Component\PropertyAccess\PropertyAccess;
+use Symfony\Component\PropertyAccess\PropertyAccessor;
+
+abstract class AbstractJson
+{
+    /** @var PropertyAccessor */
+    private $accessor;
+
+    public function __construct()
+    {
+        $this->accessor = PropertyAccess::createPropertyAccessor();
+    }
+
+    /**
+     * Get the latest json response
+     *
+     * @return stdClass decoded json into an object
+     */
+    abstract protected function getJson();
+
+    /**
+     * Get the value for a path
+     *
+     * @param string $path Path to parse
+     *
+     * @return mixed
+     * @throws AccessException path not valid
+     */
+    protected function getValue($path)
+    {
+        return $this->accessor->getValue($this->getJson(), $path);
+    }
+
+    /**
+     * @Then the response should be a valid json response
+     *
+     * ---
+     *
+     * This method is built-on the default php's json extension. You should
+     * overwrite it if you want to add supplementary checks or use something
+     * else instead (such as Seldaek's JsonLint package).
+     */
+    public function responseIsValidjson()
+    {
+        $this->getJson();
+
+        Assert::assertSame(JSON_ERROR_NONE, json_last_error(), 'The latest json response should be a valid json response');
+    }
+
+    /** @Then :path should be accessible in the latest json response */
+    public function pathShouldBeReadable($path, $default = null)
+    {
+        Assert::assertTrue($this->accessor->isReadable($this->getJson(), $path), "The path $path should be a valid path");
+    }
+
+    /** @Then :path should not exist in the latest json response */
+    public function pathShouldNotBeReadable($path)
+    {
+        Assert::assertFalse($this->accessor->isReadable($this->getJson(), $path), "The path $path should not be a valid path");
+    }
+
+    /** @Then in the json, :path should be equal to :expected */
+    public function theJsonPathShouldBeEqualTo($path, $expected)
+    {
+        Assert::assertEquals($expected, $this->getValue($path));
+    }
+
+    /** @Then in the json, :path should not be equal to :expected */
+    public function theJsonPathShouldNotBeEqualTo($path, $expected)
+    {
+        Assert::assertNotEquals($expected, $this->getValue($path));
+    }
+
+    /** @Then in the json, :path should be: */
+    public function theJsonPathShouldBePyString($path, PyStringNode $expected)
+    {
+        Assert::assertSame($expected, $this->getValue($path)->getRaw());
+    }
+
+    /** @Then /^in the json, "(?P<path>(?:[^"]|\\")*)" should be (?P<expected>true|false)$/ */
+    public function theJsonPathShouldBe($path, $expected)
+    {
+        Assert::assertSame('true' === $expected, $this->getValue($path));
+    }
+
+    /** @Then /^in the json, "(?P<path>(?:[^"]|\\")*)" should not be (?P<expected>true|false)$/ */
+    public function theJsonPathShouldNotBe($path, $expected)
+    {
+        Assert::assertNotSame('true' === $expected, $this->getValue($path));
+    }
+
+    /** @Then in the json, :path should be null */
+    public function theJsonPathShouldBeNull($path)
+    {
+        Assert::assertNull($this->getValue($path));
+    }
+
+    /** @Then in the json, :path should not be null */
+    public function theJsonPathShouldNotBeNull($path)
+    {
+        Assert::assertNotNull($this->getValue($path));
+    }
+
+    /** @Then in the json, :path should be empty */
+    public function theJsonPathShouldBeEmpty($path)
+    {
+        Assert::assertEmpty($this->getValue($path));
+    }
+
+    /** @Then in the json, :path should not be empty */
+    public function theJsonPathShouldNotBeEmpty($path)
+    {
+        Assert::assertNotEmpty($this->getValue($path));
+    }
+
+    /** @Then in the json, :path should contain :expected */
+    public function theJsonPathContains($path, $expected)
+    {
+        Assert::assertContains($expected, $this->getValue($path));
+    }
+
+    /** @Then /^in the json, :path should not contain :expected */
+    public function theJsonPathNotContains($path, $expected)
+    {
+        Assert::assertNotContains($expected, $this->getValue($path));
+    }
+
+    /** @Then in the json, :path should be a valid date(time) */
+    public function theJsonPathShouldBeAValidDate($path)
+    {
+        try {
+            new Datetime($this->getValue($path));
+        } catch (Exception $e) {
+            Assert::fail("$path does not contain a valid date");
+        }
+    }
+
+    /** @Then in the json, :path should be a valid :format formatted date(time) */
+    public function theJsonPatShouldHaveThisFormat($path, $format)
+    {
+        Assert::assertNotFalse(Datetime::createFromFormat($format, $this->getValue($path)));
+    }
+
+    /** @Then in the json, :path should be greater than :expected */
+    public function theJsonPathShouldBeGreaterThan($path, $expected)
+    {
+        Assert::assertGreaterThan((int) $expected, $this->getValue($path));
+    }
+
+    /** @Then in the json, :path should be greater than or equal to :expected */
+    public function theJsonPathShouldBeGreaterOrEqualThan($path, $expected)
+    {
+        Assert::assertGreaterThanOrEqual((int) $expected, $this->getValue($path));
+    }
+
+    /** @Then in the json, :path should be less than :expected */
+    public function theJsonPathShouldBeLessThan($path, $expected)
+    {
+        Assert::assertLessThan((int) $expected, $this->getValue($path));
+    }
+
+    /** @Then in the json, :path should be less than or equal to :expected */
+    public function theJsonPathShouldBeLessOrEqualThan($path, $expected)
+    {
+        Assert::assertLessThanOrEqual((int) $expected, $this->getValue($path));
+    }
+
+    /** @Then in the json, :path should be an array */
+    public function shouldBeAnArray($path)
+    {
+        Assert::assertInternalType('array', $this->getValue($path));
+    }
+
+    /** @Then in the json, :path should have at least :count element(s) */
+    public function theJsonPathShouldHaveAtLeastElements($path, $count)
+    {
+        $value = $this->getValue($path);
+
+        Assert::assertInternalType('array', $value);
+        Assert::assertGreaterThanOrEqual((int) $count, count($value));
+    }
+
+    /** @Then in the json, :path should have :count element(s) */
+    public function theJsonPathShouldHaveElements($path, $count)
+    {
+        Assert::assertCount((int) $count, $this->getValue($path));
+    }
+
+    /** @Then in the json, :path should have at most :count element(s) */
+    public function theJsonPathShouldHaveAtMostElements($path, $count)
+    {
+        $value = $this->getValue($path);
+
+        Assert::assertInternalType('array', $value);
+        Assert::assertLessThanOrEqual((int) $count, count($value));
+    }
+}
+
---
+++ context/Json.php
@@ -0,0 +1,60 @@
+<?php
+namespace Wisembly\Behat\Context;
+
+use DatetimeImmutable;
+use InvalidArgumentException;
+
+use PHPUnit_Framework_Assert as Assert;
+
+use Wisembly\Behat\Extension\Context\ApiTrait;
+use Wisembly\Behat\Extension\Context\ApiInterface;
+
+class Json extends AbstractJson implements ApiInterface
+{
+    use ApiTrait;
+
+    /** {@inheritDoc} */
+    protected function getJson()
+    {
+        return $this->getResponse()->json(['object' => true]);
+    }
+
+    public function responseIsValidjson()
+    {
+        Assert::assertSame('application/json', $this->getResponse()->getHeader('Content-Type', false), 'The response should have a valid content-type');
+
+        parent::responseIsValidjson();
+    }
+
+    /** @Then in the json, the date difference between :from and :to should be equal to :diff day(s) */
+    public function theDateDiffShouldBeEqualTo($from, $to, $diff, $format = 'days')
+    {
+        $to = new DatetimeImmutable($this->getValue($to));
+        $from = new DatetimeImmutable($this->getValue($from));
+
+        switch ($format) {
+            case 'days':
+                $format = '%a';
+                break;
+
+            case 'hours':
+                $format = '%h';
+                break;
+
+            case 'minutes':
+                $format = '%i';
+                break;
+
+            case 'seconds':
+                $format = '%s';
+                break;
+
+            default:
+                throw new InvalidArgumentException(sprintf('Format %s not recognized', $format));
+        }
+
+        $interval = $to->diff($from, true);
+
+        Assert::assertEquals($diff, $interval->format($format));
+    }
+}
