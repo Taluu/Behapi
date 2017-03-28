62590853314faada1266b775d01d6d28a8ff478d
Baptiste Clavié <clavie.b@gmail.com>
2017-03-28 16:33:40 +0200

    Rename DebugRest to DebugHttp

--- src/Context/DebugHttp.php
+++ src/Context/DebugHttp.php
@@ -17,10 +17,10 @@ use Behapi\Extension\Tools\Debug;
 use Behapi\Extension\Tools\LastHistory;
 
 /**
- * Debug rest
+ * Debug http
  *
  * Allows to debug a scenario, or, if the debug is activated, to print
- * a message if a scenario failed for rest requests.
+ * a message if a scenario failed for http requests.
  *
  * @author Baptiste Clavié <clavie.b@gmail.com>
  */
@@ -37,7 +37,7 @@ class DebugRest implements Context, ApiInterface
         $this->history = $history;
     }
 
-    /** @AfterScenario @api */
+    /** @AfterScenario @http */
     public function debugAfter(AfterScenarioScope $scope)
     {
         if (null === $this->debug) {
