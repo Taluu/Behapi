2f57856b2d07e7e18c38073efe281c4821512a7b
Baptiste Clavié <clavie.b@gmail.com>
2017-03-29 17:16:37 +0200

    Store more than one request / response in LastHistory

--- src/Extension/Tools/LastHistory.php
+++ src/Extension/Tools/LastHistory.php
@@ -1,6 +1,7 @@
 <?php
 namespace Behapi\Extension\Tools;
 
+use Psr\Http\Message\MessageInterface;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\ResponseInterface;
 
@@ -11,44 +12,53 @@ use Http\Client\Exception\HttpException;
 
 class LastHistory implements Journal
 {
-    /** @var RequestInterface */
-    private $lastRequest = null;
-
-    /** @var ResponseInterface */
-    private $lastResponse = null;
+    /** @var MessageInterface[][] Array of tuples of RequestInterface and ?ResponseInterface */
+    private $tuples = [];
 
     /** {@inheritDoc} */
     public function addSuccess(RequestInterface $request, ResponseInterface $response)
     {
-        $this->lastRequest = $request;
-        $this->lastResponse = $response;
+        $this->tuples[] = [$request, $response];
     }
 
     /** {@inheritDoc} */
     public function addFailure(RequestInterface $request, Exception $exception)
     {
-        $this->lastRequest = $request;
-        $this->lastResponse = null;
+        $tuple = [$request, null];
 
         if ($exception instanceof HttpException) {
-            $this->lastResponse = $exception->getResponse();
+            $tuple[1] = $exception->getResponse();
         }
+
+        $this->tuples[] = $tuple;
     }
 
     public function getLastRequest(): ?RequestInterface
     {
-        return $this->lastRequest;
+        if (1 < count($this->tuples)) {
+            return null;
+        }
+
+        $tuple = end($this->tuples);
+        reset($this->tuples);
+
+        return $tuple[0];
     }
 
     public function getLastResponse(): ?ResponseInterface
     {
-        return $this->lastResponse;
+        if (1 > count($this->tuples)) {
+            return null;
+        }
+
+        $tuple = end($this->tuples);
+        reset($this->tuples);
+
+        return $tuple[1];
     }
 
     public function reset(): void
     {
-        $this->lastRequest = null;
-        $this->lastResponse = null;
+        $this->tuples = [];
     }
 }
-
