9514bb25ad1f10a490781536463163058c2d1715
Baptiste Clavié <clavie.b@gmail.com>
2018-02-08 18:13:09 +0100

    Add EventDispatcher service in the container

---
+++ src/EventListener/Events.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types=1);
+namespace Behapi\EventListener;
+
+final class Events
+{
+    private function __construct()
+    {
+    }
+
+    private function __clone()
+    {
+    }
+}
