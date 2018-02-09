e01758da79524dcf7a0bc07642ad70411ac9f8fa
Baptiste Clavié <clavie.b@gmail.com>
2018-02-09 21:01:08 +0100

    Bump assert dependency

--- src/Tools/Assert.php
+++ src/Tools/Assert.php
@@ -6,26 +6,13 @@ use Webmozart\Assert\Assert as webmozart;
 /**
  * Assert
  *
- * Use while https://github.com/webmozart/assert/pull/37 isn't merged
+ * Use while https://github.com/webmozart/assert/pull/58 isn't merged
  *
- * @method static void nullOrNotContains($value, $subString, $message = '')
- * @method static void allContains($values, $subString, $message = '')
+ * @method static void nullOrNotRegx($value, $pattern, $message = '')
+ * @method static void allNotRegex($values, $pattern, $message = '')
  */
 final class Assert extends webmozart
 {
-    public static function notContains($value, $subString, $message = '')
-    {
-        if (false === strpos($value, $subString)) {
-            return;
-        }
-
-        static::reportInvalidArgument(sprintf(
-            $message ?: 'Expected a value to not contain %2$s. Got: %s',
-            static::valueToString($value),
-            static::valueToString($subString)
-        ));
-    }
-
     public static function notRegex($value, $pattern, $message = '')
     {
         if (!preg_match($pattern, $value)) {
