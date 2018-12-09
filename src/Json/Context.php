e2d6e833fb20c72ac6a8bff11ac7aa49c3de29af
Baptiste Clavié <clavie.b@gmail.com>
2018-12-09 15:55:24 +0100

    Merge abstract json context and provided json context

--- src/Json/Context.php
+++ src/Json/Context.php
@@ -2,39 +2,180 @@
 namespace Behapi\Json;
 
 use stdClass;
+use DateTimeImmutable;
+
+use Throwable;
 use InvalidArgumentException;
+
+use Behat\Gherkin\Node\PyStringNode;
+use Behat\Behat\Context\Context as BehatContext;
+
+use Symfony\Component\PropertyAccess\PropertyAccess;
+use Symfony\Component\PropertyAccess\PropertyAccessor;
 
 use Webmozart\Assert\Assert;
 
 use Behapi\HttpHistory\History as HttpHistory;
 
 use function sprintf;
+use function preg_match;
 
 use function json_decode;
 use function json_last_error;
 use function json_last_error_msg;
 
 use const JSON_ERROR_NONE;
+use const PREG_OFFSET_CAPTURE;
 
-class Context extends AbstractContext
+class Context implements BehatContext
 {
+    use CountTrait;
+    use RegexTrait;
+    use ComparisonTrait;
+    use EachInCollectionTrait;
+
     /** @var HttpHistory */
     private $history;
 
     /** @var string[] */
     private $contentTypes;
 
+    /** @var PropertyAccessor */
+    private $accessor;
+
     public function __construct(HttpHistory $history, array $contentTypes = ['application/json'])
     {
-        parent::__construct();
+        $this->accessor = PropertyAccess::createPropertyAccessor();
 
         $this->history = $history;
         $this->contentTypes = $contentTypes;
     }
 
-    protected function getJson()
+    /** @return mixed */
+    protected function getValue(?string $path)
     {
-        return json_decode((string) $this->history->getLastResponse()->getBody());
+        $json = json_decode((string) $this->history->getLastResponse()->getBody());
+
+        return $path === null ? $json : $this->accessor->getValue($json, $path);
+    }
+
+    /** @Then :path should be accessible in the latest json response */
+    final public function path_should_be_readable(string $path): void
+    {
+        Assert::true($this->accessor->isReadable($this->getValue(null), $path), "The path $path should be a valid path");
+    }
+
+    /** @Then :path should not exist in the latest json response */
+    final public function path_should_not_be_readable(string $path): void
+    {
+        Assert::false($this->accessor->isReadable($this->getValue(null), $path), "The path $path should not be a valid path");
+    }
+
+    /**
+     * @Then in the json, :path should be equal to :expected
+     * @Then in the json, :path should :not be equal to :expected
+     */
+    final public function the_json_path_should_be_equal_to(string $path, ?string $not = null, $expected): void
+    {
+        $assert = [Assert::class, $not !== null ? 'notEq' : 'eq'];
+        assert(is_callable($assert));
+
+        $assert($this->getValue($path), $expected);
+    }
+
+    /** @Then in the json, :path should be: */
+    final public function the_json_path_should_be_py_string(string $path, PyStringNode $expected): void
+    {
+        Assert::same($this->getValue($path), $expected->getRaw());
+    }
+
+    /**
+     * @Then /^in the json, "(?P<path>(?:[^"]|\\")*)" should be (?P<expected>true|false)$/
+     * @Then /^in the json, "(?P<path>(?:[^"]|\\")*)" should :not be (?P<expected>true|false)$/
+     */
+    final public function the_json_path_should_be(string $path, ?string $not = null, string $expected): void
+    {
+        $assert = [Assert::class, $not !== null ? 'notSame' : 'same'];
+        assert(is_callable($assert));
+
+        $assert($this->getValue($path), 'true' === $expected);
+    }
+
+    /**
+     * @Then in the json, :path should be null
+     * @Then in the json, :path should :not be null
+     */
+    final public function the_json_path_should_be_null(string $path, ?string $not = null): void
+    {
+        $assert = [Assert::class, $not !== null ? 'notNull' : 'null'];
+        assert(is_callable($assert));
+
+        $assert($this->getValue($path));
+    }
+
+    /**
+     * @Then in the json, :path should be empty
+     * @Then in the json, :path should :not be empty
+     */
+    final public function the_json_path_should_be_empty(string $path, ?string $not = null): void
+    {
+        $assert = [Assert::class, $not !== null ? 'notEmpty' : 'isEmpty'];
+        assert(is_callable($assert));
+
+        $assert($this->getValue($path));
+    }
+
+    /**
+     * @Then in the json, :path should contain :expected
+     * @Then in the json, :path should :not contain :expected
+     */
+    final public function the_json_path_contains(string $path, ?string $not = null, $expected): void
+    {
+        $assert = [Assert::class, $not !== null ? 'notContains' : 'contains'];
+        assert(is_callable($assert));
+
+        $assert($this->getValue($path), $expected);
+    }
+
+    /** @Then in the json, :path collection should contain an element with :value equal to :expected */
+    final public function the_json_path_collection_contains(string $path, string $value, $expected): void
+    {
+        $collection = $this->getValue($path);
+        Assert::isIterable($collection);
+
+        foreach ($collection as $element) {
+            if ($expected === $this->accessor->getValue($element, $value)) {
+                return;
+            }
+        }
+
+        throw new InvalidArgumentException("$path collection does not contain an element with $value equal to $expected");
+    }
+
+    /** @Then in the json, :path should be a valid date(time) */
+    final public function the_json_path_should_be_a_valid_date(string $path): void
+    {
+        try {
+            new DateTimeImmutable($this->getValue($path));
+        } catch (Throwable $t) {
+            throw new InvalidArgumentException("$path does not contain a valid date");
+        }
+    }
+
+    /**
+     * @Then in the json, the root should be an array
+     * @Then in the json, :path should be an array
+     */
+    final public function should_be_an_array(?string $path = null): void
+    {
+        Assert::isArray($this->getValue($path));
+    }
+
+    /** @Then in the json, :path should be a valid json encoded string */
+    final public function the_json_path_should_be_a_valid_json_encoded_string(string $path): void
+    {
+        json_decode($this->getValue($path));
+        Assert::same(json_last_error(), JSON_ERROR_NONE);
     }
 
     /**
@@ -48,7 +189,7 @@ class Context extends AbstractContext
      */
     public function response_should_be_a_valid_json_response()
     {
-        $this->getJson();
+        $this->getValue(null);
 
         [$contentType,] = explode(';', $this->history->getLastResponse()->getHeaderLine('Content-Type'), 2);
 
