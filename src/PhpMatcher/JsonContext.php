6b519262e2c0e6b59a13294b1efbed8691d8d191
Baptiste Clavié <clavie.b@gmail.com>
2020-02-05 10:22:52 +0100

    Remove redundant steps for php matcher context
    Use the full json instead...

--- src/PhpMatcher/JsonContext.php
+++ src/PhpMatcher/JsonContext.php
@@ -1,20 +1,15 @@
 <?php declare(strict_types=1);
 namespace Behapi\PhpMatcher;
 
-use stdClass;
 use InvalidArgumentException;
 
 use Behat\Behat\Context\Context;
 use Behat\Gherkin\Node\PyStringNode;
 
-use Symfony\Component\PropertyAccess\PropertyAccess;
-use Symfony\Component\PropertyAccess\PropertyAccessor;
-
 use Behapi\HttpHistory\History as HttpHistory;
 
 use function sprintf;
-use function json_encode;
-use function json_decode;
+use function is_string;
 
 class JsonContext implements Context
 {
@@ -24,14 +19,10 @@ class JsonContext implements Context
     /** @var MatcherFactory */
     private $factory;
 
-    /** @var PropertyAccessor */
-    private $accessor;
-
     public function __construct(HttpHistory $history)
     {
         $this->history = $history;
         $this->factory = new MatcherFactory;
-        $this->accessor = PropertyAccess::createPropertyAccessor();
     }
 
     /** @Then the root should match: */
@@ -39,73 +30,18 @@ class JsonContext implements Context
     {
         $matcher = $this->factory->createMatcher();
 
-        if ($matcher->match($this->getJson(), $pattern->getRaw())) {
+        if ($matcher->match((string) $this->history->getLastResponse()->getBody(), $pattern->getRaw())) {
             return;
         }
+
+        $error = $matcher->getError();
+        assert(is_string($error));
 
         throw new InvalidArgumentException(
             sprintf(
                 'The json root does not match with the given pattern (error : %s)',
-                $matcher->getError()
+                $error
             )
         );
-    }
-
-    /** @Then in the json, :path should match: */
-    final public function path_should_match(string $path, PyStringNode $pattern): void
-    {
-        $value = $this->getValue($path);
-        $matcher = $this->factory->createMatcher();
-
-        $json = json_encode($value);
-
-        if ($matcher->match($json, $pattern->getRaw())) {
-            return;
-        }
-
-        throw new InvalidArgumentException(
-            sprintf(
-                'The json path "%s" does not match with the given pattern (error : %s)',
-                $path,
-                $matcher->getError()
-            )
-        );
-    }
-
-    /** @Then in the json, :path should not match: */
-    final public function path_should_not_match(string $path, PyStringNode $pattern): void
-    {
-        $value = $this->getValue($path);
-        $matcher = $this->factory->createMatcher();
-
-        $json = json_encode($value);
-
-        if (!$matcher->match($json, $pattern->getRaw())) {
-            return;
-        }
-
-        throw new InvalidArgumentException(
-            sprintf(
-                'The json path "%s" matches with the given pattern (error : %s)',
-                $path,
-                $matcher->getError()
-            )
-        );
-    }
-
-    private function getJson(): ?stdClass
-    {
-        return json_decode((string) $this->history->getLastResponse()->getBody());
-    }
-
-    private function getValue(string $path)
-    {
-        $json = $this->getJson();
-
-        if (null === $json) {
-            throw new InvalidArgumentException('Expected a Json valid content, got none');
-        }
-
-        return $this->accessor->getValue($json, $path);
     }
 }
